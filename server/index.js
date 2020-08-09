bc61c6c6d4a9dd645bb64d296a4e83fbe206178a
Connor Robetorye <crobetorye@gmail.com>
2020-08-08 17:09:38 -0700

    update cors for production

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 //  Ref: https://medium.com/@alexishevia/using-cors-in-express-cac7e29b005b
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://covidsimple.herokuapp.com/",
     })
 );
 
