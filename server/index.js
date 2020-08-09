9d4c046094749565ab6b1db06b26afd58f8d9fd9
Connor Robetorye <crobetorye@gmail.com>
2020-08-09 03:27:17 -0700

    helmet tweak

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(
         "script-src": ["'Self'", "'unsafe-inline'"],
         "font-src": ["'Self'", "fonts.gstatic.com"],
         "style-src": ["'Self'", "'unsafe-inline'", "fonts.googleapis.com"],
-        "img-src": ["'Self'", "*.githubusercontent.com"]
+        "img-src": ["'Self'", "*.githubusercontent.com", ":data"]
       },
     })
   );
